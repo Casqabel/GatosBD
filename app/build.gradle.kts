34413fb09504788a77ee642817e204ddb5f04abf
profe <cascabeltb@gmail.com>
2025-11-12 16:07:07 +0100

    Arreglado gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,9 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation("io.coil-kt.coil3:coil-compose:3.2.0")
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.2.0")
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.preference)
-    implementation(libs.material)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
